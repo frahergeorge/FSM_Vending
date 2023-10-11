85f0cdccf55eeffed6dff4b22d355b6998beb26a
MichaelRyan23 <142366498+MichaelRyan23@users.noreply.github.com>
2023-10-11 16:53:14 -0500

    Update nextStateLogic.sv maybe fixed er?

--- nextStateLogic.sv
+++ nextStateLogic.sv
@@ -1,7 +1,9 @@
 module nextStateLogic(
-	input logic [3:0] S, [1:0] EO,	
-	output logic [3:0] Z		// next state outputs
+	input logic [1:0] EO,		// encoded outputs/not a zero
+	input logic [3:0] S,		// current state bit logic
+	output logic [3:0] Z		//Values going into D flip flops 
 );
+
 
 // S[2] = left most bit (MSB)
 // S[1] = second left most bit
@@ -9,8 +11,8 @@ module nextStateLogic(
 // EO[1] = Z1 (from encoder)
 // EO[0] = Z0 (from encoder)
 	assign Z[0] = (~S[0] & ~EO[1] & EO[0]) | (~S[2] & S[0] & ~EO[0]) | (~S[2] & S[0] & EO[1]) | (S[2] & ~EO[1] & EO[0]);
-	assign Z[1] = (~S[1] & EO[1] & ~EO[0]) | (S[2] & EO[1] & ~EO[0]) | (~S[2] & S[1] & ~S[0] & ~E[1]) | (~S[2] & S[1] & ~EO[1] & ~EO[0]) | (~S[2] & S[1] & EO[1] & EO[0]) | (~S[2] & ~S[1] & S[0] & ~EO[1] & EO[0]);
-	assign Z[2] = (~S[2] & EO[1] & EO[0]) | (S[0] & EO[1] & EO[1]) | (~S[2] & S[1] & EO[1]) | (S[1] & EO[1] & EO[0]) | (~S[2] & S[1] & S[0] & EO[0]) | (S[2] & ~S[1] & ~S[0] & ~EO[1]) | (S[2] & ~S[1] & ~S[0] & ~EO[0]);
+	assign Z[1] = (~S[1] & EO[1] & ~EO[0]) | (S[2] & EO[1] & ~EO[0]) | (~S[2] & S[1] & ~S[0] & ~EO[1]) | (~S[2] & S[1] & ~EO[1] & ~EO[0]) | (~S[2] & S[1] & EO[1] & EO[0]) | (~S[2] & ~S[1] & S[0] & ~EO[1] & EO[0]);
+	assign Z[2] = (~S[2] & EO[1] & EO[0]) | (S[0] & EO[1] & EO[0]) | (~S[2] & S[1] & EO[1]) | (S[1] & EO[1] & EO[0]) | (~S[2] & S[1] & S[0] & EO[0]) | (S[2] & ~S[1] & ~S[0] & ~EO[1]) | (S[2] & ~S[1] & ~S[0] & ~EO[0]);	
 	assign Z[3] = S[2] & ~S[1] & ~S[0] & EO[1] & EO[0];
 
 endmodule
